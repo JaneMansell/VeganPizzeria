fe9ff2c55c504e47af4164aa154d7df776e3ba61
victoriadw <victoria.daramywilliams@gmail.com>
2023-05-28 13:08:31 +0100

    Initial commit

---
+++ src/main/java/com/Plantizza/VeganPizzeria/VeganPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.Plantizza.VeganPizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Plantizza/VeganPizzeria/VeganPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Plantizza.VeganPizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
