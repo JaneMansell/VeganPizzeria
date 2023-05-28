0748ee37b7e5dfcad4aa9f60f006fc13b5f5f70a
JaneMansell <mrsjanemansell@gmail.com>
2023-05-28 14:00:14 +0100

    Add email addresses to data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 USE veganPizzeria;
 
 INSERT INTO customers VALUES
-(1,'Judd Trump', '1 Crucible Street', 'PO32 5JW', '07777123456'),
-(2,'Neil Robertson', '3 Crucible Street', 'PO32 5JS', '07777654321'),
-(3,'Luca Brecel', '5 Crucible Street', 'PO32 5JW', '07777111111'),
-(4,'Mark Selby', '1 Ally Pally Street', 'PO32 6JL', '07777122222'),
-(5,'Jimmy White', '3 Ally Pally Street', 'PO32 6JW', '07777123333'),
-(6,'Dominic Dale', '5 Ally Pally Street', 'PO32 6JK', '07777123444'),
-(7,'Yan Bingtao', '7 Ally Pally Street', 'PO32 6JW', '07777123455'),
-(8,'Marco Fu', '9 Ally Pally Street', 'PO32 6JW', '07777222222');
+(1,'Judd Trump', 'judd@gmail.com', '1 Crucible Street', 'PO32 5JW', '07777123456'),
+(2,'Neil Robertson', 'neil@btinternet.com','3 Crucible Street', 'PO32 5JS', '07777654321'),
+(3,'Luca Brecel', 'luca@gmail.com','5 Crucible Street', 'PO32 5JW', '07777111111'),
+(4,'Mark Selby', 'mark@plusnet.com','1 Ally Pally Street', 'PO32 6JL', '07777122222'),
+(5,'Jimmy White', 'jimmy@gmail.com','3 Ally Pally Street', 'PO32 6JW', '07777123333'),
+(6,'Dominic Dale', 'dom@gmail.com','5 Ally Pally Street', 'PO32 6JK', '07777123444'),
+(7,'Yan Bingtao', 'yan@@btinternet.com','7 Ally Pally Street', 'PO32 6JW', '07777123455'),
+(8,'Marco Fu', 'marco@gmail.com','9 Ally Pally Street', 'PO32 6JW', '07777222222');
 
 INSERT INTO pizzas VALUES
 ('Plain', 6.99),
