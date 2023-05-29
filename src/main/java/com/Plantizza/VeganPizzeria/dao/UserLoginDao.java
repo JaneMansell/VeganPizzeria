4f427d1c7252d5a2c9ec2de27ec0cce34b216d17
JaneMansell <mrsjanemansell@gmail.com>
2023-05-29 12:18:35 +0100

    Add CRUD for user login

--- src/main/java/com/Plantizza/VeganPizzeria/dao/UserLoginDao.java
+++ src/main/java/com/Plantizza/VeganPizzeria/dao/UserLoginDao.java
@@ -5,9 +5,9 @@ import com.Plantizza.VeganPizzeria.entities.UserLogin;
 import java.util.List;
 
 public interface UserLoginDao {
-    UserLogin getuserLoginByID(int id);
+    UserLogin getuserLoginByEmailAddress(String email);
     List<UserLogin> getAlluserLogins();
     UserLogin adduserLogin(UserLogin userLogin);
     void updateuserLogin(UserLogin userLogin);
-    void deleteuserLoginById(int id);
+    void deleteuserLoginByEmailAddress(String email);
 }
--- src/main/java/com/Plantizza/VeganPizzeria/dao/UserLoginDaoDB.java
+++ src/main/java/com/Plantizza/VeganPizzeria/dao/UserLoginDaoDB.java
@@ -2,6 +2,7 @@ package com.Plantizza.VeganPizzeria.dao;
 
 import com.Plantizza.VeganPizzeria.entities.UserLogin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -18,29 +19,50 @@ public class UserLoginDaoDB implements UserLoginDao{
     JdbcTemplate jdbc;
 
     @Override
-    public UserLogin getuserLoginByID(int id) {
-        return null;
+    public UserLogin getuserLoginByEmailAddress(String email) {
+        try{
+            final String GET_LOGIN_BY_EMAIL = "SELECT * FROM login WHERE emailAddress = ?";
+            return jdbc.queryForObject(GET_LOGIN_BY_EMAIL, new UserLoginMapper(), email);
+        } catch(DataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public List<UserLogin> getAlluserLogins() {
-        return null;
+        final String GET_ALL_LOGINS = "SELECT * FROM login";
+        return jdbc.query(GET_ALL_LOGINS, new UserLoginMapper());
     }
 
     @Override
     @Transactional
     public UserLogin adduserLogin(UserLogin userLogin) {
-        return null;
+        final String INSERT_LOGIN ="INSERT INTO login(" +
+                "emailAddress, password," +
+                "userType) VALUES (?,?,?)";
+        jdbc.update(INSERT_LOGIN,
+                userLogin.getEmailAddress(),
+                userLogin.getPassword(),
+                userLogin.getUserType());
+
+        return userLogin;
     }
 
     @Override
     public void updateuserLogin(UserLogin userLogin) {
-
+        final String UPDATE_LOGIN = "UPDATE login SET " +
+                "password =?, userType = ? WHERE emailAddress = ?";
+        jdbc.update(UPDATE_LOGIN,
+                userLogin.getPassword(),
+                userLogin.getUserType(),
+                userLogin.getEmailAddress());
     }
 
     @Override
     @Transactional
-    public void deleteuserLoginById(int id) {
+    public void deleteuserLoginByEmailAddress(String email) {
+        final String DELETE_LOGIN ="DELETE FROM login WHERE emailAddress = ?";
+        jdbc.update(DELETE_LOGIN, email);
 
     }
 
