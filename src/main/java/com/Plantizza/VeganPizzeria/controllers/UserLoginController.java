5d0e7aa3e1549b1dcb32eab59ca2c4a73774e926
JaneMansell <mrsjanemansell@gmail.com>
2023-05-31 14:21:12 +0100

    Add login page

--- src/main/java/com/Plantizza/VeganPizzeria/controllers/UserLoginController.java
+++ src/main/java/com/Plantizza/VeganPizzeria/controllers/UserLoginController.java
@@ -3,8 +3,13 @@ package com.Plantizza.VeganPizzeria.controllers;
 import com.Plantizza.VeganPizzeria.dao.CustomerDao;
 import com.Plantizza.VeganPizzeria.dao.EmployeeDao;
 import com.Plantizza.VeganPizzeria.dao.UserLoginDao;
+import com.Plantizza.VeganPizzeria.entities.UserLogin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class UserLoginController {
@@ -17,4 +22,6 @@ public class UserLoginController {
 
     @Autowired
     EmployeeDao employeeDao;
+
+
 }
