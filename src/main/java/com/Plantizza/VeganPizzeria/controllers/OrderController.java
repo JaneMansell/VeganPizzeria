0eb16737ddae59a0b240700e18a8c9672a8673da
JaneMansell <mrsjanemansell@gmail.com>
2023-06-04 08:58:30 +0100

    Add duck pictures to tracking screen using API

--- src/main/java/com/Plantizza/VeganPizzeria/controllers/OrderController.java
+++ src/main/java/com/Plantizza/VeganPizzeria/controllers/OrderController.java
@@ -3,18 +3,28 @@ package com.Plantizza.VeganPizzeria.controllers;
 import com.Plantizza.VeganPizzeria.dao.CustomerDao;
 import com.Plantizza.VeganPizzeria.dao.OrderDao;
 import com.Plantizza.VeganPizzeria.dao.OrderLineDao;
+import com.Plantizza.VeganPizzeria.entities.DuckPic;
 import com.Plantizza.VeganPizzeria.entities.Order;
 import com.Plantizza.VeganPizzeria.entities.OrderLine;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -31,7 +41,7 @@ public class OrderController {
     OrderLineDao orderLineDao;
 
     @GetMapping("customerTrackOrder")
-    public String displayStatusToCustomer(@RequestParam(name = "id") String id, Model model) {
+    public String displayStatusToCustomer(@RequestParam(name = "id") String id, Model model) throws IOException, InterruptedException {
 
         LocalDate today = LocalDate.now();
         int customerId = Integer.parseInt(id);
@@ -64,6 +74,22 @@ public class OrderController {
             }
         }
         model.addAttribute("orders", orders);
+        // Animal Fact from API
+        //Build get request
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://random-d.uk/api/random"))
+                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .build();
+        //Get response from endpoint
+        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        //Turn response body into a json String
+        String responseString = response.body().toString();
+        //Use Google Gson to map the json string to a java object
+        //This required an extra dependency in the pom file
+        Gson gson = new Gson();
+        DuckPic duckpic = gson.fromJson(responseString, DuckPic.class);
+        //Add duckpic object to model. Use Thymeleaf in html to get URL.
+        model.addAttribute("duckpic", duckpic);
 
         return "customerTrackOrder";
     }
